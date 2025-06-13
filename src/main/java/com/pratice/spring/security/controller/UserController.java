b09020f1ab309f1d904dc2e9f7ad48d35d31558e
Piyush Dewangan <dewanganpiyush999@gmail.com>
2025-06-13 10:44:08 +0530

    POST login api added

--- src/main/java/com/pratice/spring/security/controller/UserController.java
+++ src/main/java/com/pratice/spring/security/controller/UserController.java
@@ -21,7 +21,7 @@ public class UserController {
 
     @PostMapping("/login")
     public String login(@RequestBody Users user){
-        return "Success";
+        return service.verify(user);
     }
 
 }
