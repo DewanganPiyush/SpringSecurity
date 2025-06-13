0412520efdae780bd75ee6170a13cde766125dfc
Piyush Dewangan <dewanganpiyush999@gmail.com>
2025-06-13 15:57:23 +0530

    Full JWT token added

--- src/main/java/com/pratice/spring/security/config/SecurityConfig.java
+++ src/main/java/com/pratice/spring/security/config/SecurityConfig.java
@@ -18,25 +18,30 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Autowired
+    private JwtFilter jwtFilter;
+
+    @Autowired
     private UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         return http
                 .csrf(csrf -> csrf.disable())
-                //.authorizeHttpRequests(request -> request.anyRequest().authenticated())
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("register","login")
                         .permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
