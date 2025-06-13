fb4d2ea2cc8c0aeaf5bd425ba1936190f2b709cb
Piyush Dewangan <dewanganpiyush999@gmail.com>
2025-06-13 11:45:32 +0530

    JWT Token added

--- src/main/java/com/pratice/spring/security/service/JWTService.java
+++ src/main/java/com/pratice/spring/security/service/JWTService.java
@@ -1,11 +1,57 @@
 package com.pratice.spring.security.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Service
 public class JWTService {
 
-    public String generateToken(){
-        return "dias62q922asbdsddasahlazs";
+    private String secretkey = "CF78IA";
+
+    public JWTService() {
+
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey sk = keyGen.generateKey();
+            secretkey = Base64.getEncoder().encodeToString(sk.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+
+    public String generateToken(String username){
+
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 30))
+                .and()
+                .signWith(getKey())
+                .compact();
+
+    }
+
+    private Key getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretkey);
+        return Keys.hmacShaKeyFor(keyBytes);
+
+    }
+
 }
